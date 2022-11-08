778251b8f88ce392d98b72f3e20a70bb87992b11
blazewtt <54437675+blazewtt@users.noreply.github.com>
2022-11-08 13:08:43 +0100

    Start scene, loop fix, levels init
    Werken op school -> main branch

--- js/main.js
+++ js/main.js
@@ -2,11 +2,14 @@
 const levelOneDoor = document.querySelector("#level-one");
 const levelTwoDoor = document.querySelector("#level-two");
 const levelThreeDoor = document.querySelector("#level-three");
-const video = document.querySelector('video');
-const source = document.querySelector("video > source")
+const startVideo = document.querySelector('#start-video');
+const video = document.querySelector('#game-wrap video');
+const source = document.querySelector("#game-wrap video source");
 const startBtn = document.querySelector('#start-btn');
 const startDiv = document.querySelector('#start');
 const gameDiv = document.querySelector('#game');
+const loadVid = document.querySelector('#load-in-vid')
+const loadSrc = document.querySelector('#load-in-vid source')
 
 let gameStage;
 
@@ -18,9 +21,17 @@ const start = () => {
     gameStage = 'select'
 }
 
+const replayStart = () => {
+    startVideo.play()
+}
+
+const loadVideo = (file=text) => {
+    loadSrc.attributes.src.nodeValue = `videos/${file}.webm`;
+    loadVid.load()
+}
+
 // Functions that make the videos play and stop playing properly
 const videoLevelOne = () => {
-    console.log(source.attributes)
     source.attributes.src.nodeValue = 'videos/enter1.webm';
     video.load();
     video.play();
@@ -46,10 +57,13 @@ const videoEnded = () => {
         video.play();
     } else if (gameStage == "level1") {
         enterLevelOne();
+        hideDoors();
     } else if (gameStage == "level2") {
         enterLevelTwo();
+        hideDoors();
     } else if (gameStage == "level3") {
         enterLevelThree();
+        hideDoors();
     }
 }
 
@@ -67,12 +81,19 @@ const enterLevelThree = () => {
     console.log("enter level 3");
 }
 
+// Hide the doors after the video has ended
+const hideDoors = () => {
+    levelOneDoor.style.display = 'none';
+    levelTwoDoor.style.display = 'none';
+    levelThreeDoor.style.display = 'none';
+}
 
 // executed after DOM loaded
 levelOneDoor.addEventListener("click", videoLevelOne);
 levelTwoDoor.addEventListener("click", videoLevelTwo);
 levelThreeDoor.addEventListener("click", videoLevelThree);
-startBtn.addEventListener("click", start)
 video.addEventListener("ended", videoEnded);
+startVideo.addEventListener('ended', replayStart);
+startVideo.addEventListener('click', start);
 startDiv.style.display = 'block'
 gameDiv.style.display = 'none'
