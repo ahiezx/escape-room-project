ccd1568d6bad8662e54fdd396cd44a491869e772
blazewtt <ahmad.ahmapro2@gmail.com>
2022-11-09 19:49:23 +0100

    Level one hitboxes

--- js/levels/level0.js
+++ js/levels/level0.js
@@ -1,6 +1,41 @@
+const clock = document.querySelector("#clock");
+const letter = document.querySelector("#letter");
+const hand = document.querySelector("#hand");
+
+
+const hideLevelOneItems = () => {
+    clock.style.display = "none";
+    letter.style.display = "none";
+    hand.style.display = "none";
+};
+
+const showItems = () => {
+    clock.style.display = "block";
+    letter.style.display = "block";
+    hand.style.display = "block";
+};
+
+
 const levelStart = () => {
-    // alert('Welcome to the game! You are in a forest. You can go left or right.')
     gameStage = 'level1'
+    
     changeHint('What does the hand point to?')
-    // showDoors()
 }
+
+const chosenLetter = () => {
+    console.log('chosenLetter()')
+}
+
+const chosenClock = () => {
+    console.log('chosenClock()')
+}
+
+const chosenHand = () => {
+    console.log('chosenHand()')
+}
+
+showItems();
+
+clock.addEventListener("click", chosenClock);
+letter.addEventListener("click", chosenLetter);
+hand.addEventListener("click", chosenHand);
